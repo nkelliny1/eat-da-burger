af38410f5a82e0fe34b96a5449ffe49ed04e679f
nkelliny1 <nkelliny@outlook.com>
2020-03-24 18:46:41 -0400

    Initial commit

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection ="";
+
+if(process.env.JAWDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "",
   database: "burger_db"
-});
-
+})
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
